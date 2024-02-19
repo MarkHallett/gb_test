b0d6f744809cb13922be67bb3046f3682dea7b38
Mark <gb@markhallett.co.uk>
2024-02-19 19:08:59 +0000

    vb_aa

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@
 
 if __name__ == '__main__':
     print('hello all!!')
-    print('second change in py')
+    print('second change in py, latest Feb')
--- hello2.py
+++ hello2.py
@@ -4,3 +4,4 @@ print('hello')
 print('post branch cleanup')
 print('simple change in github')
 print('simple change in github Feb1')
+x = 10 # update first then virtual branch
